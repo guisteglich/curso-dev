5cd13bfabfddbdc54c409e0c77d5f55f8d6b7f22
guilherme <guilhermesi@furg.br>
2020-11-23 16:19:24 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template  
+
+app = Flask(__name__)
+
+
+class Cursos:
+    def __init__(self, nome, duracao, unidade):
+        self.nome = nome
+        self.duracao = duracao
+        self.unidade = unidade
+
+
+@app.route("/home")
+def main():
+    si = Cursos("Sistemas de informação", "4 anos", "C3")
+    ec = Cursos("Engenharia da computação", "5 anos", "C3")
+    ea = Cursos("Engenharia de automação", "5 anos", "C3")
+
+    lista_cursos = [si, ec, ea]
+    return render_template("index.html", titulo="Cursos", Cursos=lista_cursos)
+
+if __name__ == "__main__":
+    app.run(debug=True)
